07fa2f44a8fe60c26e2be819ad4c5406f3239152
fusion809 <brentonhorne77@gmail.com>
2016-04-25 13:06:00 +1000

    Adding debian-build script

--- lib/centos-build.sh
+++ lib/centos-build.sh
@@ -3,6 +3,5 @@ function centos-build {
   curl -sL https://rpm.nodesource.com/setup_5.x | sudo bash -     # Get the latest version of Node.js
   sudo yum install -y python nodejs-devel npm gcc gcc-c++ make \  # Install the dependencies
     glibc-devel git-core
-
   atom-build
 }

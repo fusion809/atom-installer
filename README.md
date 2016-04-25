b907e96d221ac7ada3eb65a891fad1a5fce0b66d
fusion809 <brentonhorne77@gmail.com>
2016-04-25 11:11:35 +1000

    Updating

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Atom Installer
+This project's stated purpose is to create shell scripts for automating the installation process of Atom on 32-bit and 64-bit Linux. The structure of this repository is:
+
+```bash
+.
+├── lib
+│   ├── atom-editor-aur.sh
+│   └── test.sh
+├── i686
+│   ├── arch.sh
+│   ├── centos.sh
+│   ├── debian.sh
+│   ├── fedora.sh
+│   └── gentoo.sh
+├── x86_64
+│   ├── arch.sh
+│   ├── centos.sh
+│   ├── debian.sh
+│   ├── fedora.sh
+│   └── gentoo.sh
+├── installer.sh
+├── LICENSE
+└── README.md
+```
--- x86_64/centos.sh
+++ x86_64/centos.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+printf "Do you want to install Atom from A) a binary package or would you prefer B) a source install? [A/B] "
+read preference
+
+if [[ $preference == "A" ]]; then
+  curl -sL https://atom.io/download/rpm > atom.x86_64.rpm
+  sudo yum install -y atom.x86_64.rpm
+elif [[ $preference == "B" ]]; then
+  # Get dependencies
+  curl -sL https://rpm.nodesource.com/setup_5.x | sudo bash -
+  sudo yum install -y python nodejs-devel npm gcc gcc-c++ make glibc-devel git-core
+
+  # Get the source code
+  git clone https://github.com/atom/atom
+  cd atom
+  git fetch -p
+  git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+  script/build
+
+  printf "Do you want to install Atom to a local directory or system-wide? [local/system] "
+  read DEST_TYPE
+  printf "Where do you want to install Atom to? "
+  read DEST
+
+  if [[ $DEST_TYPE == "local" ]]; then
+    script/grunt install --channel=stable --install-dir $DEST
+  elif [[ $DEST_TYPE == "system" ]]; then
+    sudo script/grunt install --channel=stable --install-dir $DEST
+  fi
+fi

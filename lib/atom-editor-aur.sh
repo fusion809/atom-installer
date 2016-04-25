5f8cf0a7f366708caa8ff42025d17059a2b7888e
fusion809 <brentonhorne77@gmail.com>
2016-04-25 10:44:51 +1000

    Updating

--- lib/atom-editor-aur.sh
+++ lib/atom-editor-aur.sh
@@ -1,9 +1,74 @@
 #!/bin/bash
 
 function atomin {
-  cd /tmp/atom-editor
+  if [[ -n "$1" ]]; then
+    cd /tmp/atom-editor-$1
+  else
+    cd /tmp/atom-editor
+  fi
   makepkg -si --noconfirm
   cd .
 }
 
 export -f atomin
+
+function atomaur {
+  if [[ -n "$1" ]]; then                     # if input provided; allowed input bin, git
+
+    if comex yaourt; then                    # Install with yaourt if possible
+
+      yaourt -S atom-editor-$1 --noconfirm
+
+    elif comex git; then                      # Install with git and makepkg otherwise
+
+      git clone $GIT/atom-editor-$1.git /tmp/atom-editor-$1
+      atomin $1
+
+    elif comex curl; then                     # Install with curl and makepkg otherwise
+
+      curl -sL $AUR/atom-editor-$1.tar.gz | tar xz -C /tmp
+      atomin $1
+
+    elif comex wget; then                     # Install with wget and makepkg otherwise
+
+      wget -cqO- $AUR/atom-editor-$1.tar.gz | tar xz -C /tmp
+      atomin $1
+
+    else                                      # Install curl and install with curl and makepkg otherwise
+
+      sudo pacman -S curl --noconfirm
+      curl -sL $AUR/atom-editor-$1.tar.gz | tar xz -C /tmp
+      atomin $1
+
+    fi
+  else
+    if comex yaourt; then                    # Install with yaourt if possible
+
+      yaourt -S atom-editor --noconfirm
+
+    elif comex git; then                      # Install with git and makepkg otherwise
+
+      git clone $GIT/atom-editor.git /tmp/atom-editor
+      atomin
+
+    elif comex curl; then                     # Install with curl and makepkg otherwise
+
+      curl -sL $AUR/atom-editor.tar.gz | tar xz -C /tmp
+      atomin
+
+    elif comex wget; then                     # Install with wget and makepkg otherwise
+
+      wget -cqO- $AUR/atom-editor.tar.gz | tar xz -C /tmp
+      atomin
+
+    else                                      # Install curl and install with curl and makepkg otherwise
+
+      sudo pacman -S curl --noconfirm
+      curl -sL $AUR/atom-editor.tar.gz | tar xz -C /tmp
+      atomin
+
+    fi
+  fi
+}
+
+export -f atomaur
--- x86_64/arch.sh
+++ x86_64/arch.sh
@@ -8,45 +8,13 @@ if comex atom; then                       # Check if Atom is already installed
 
 else
 
-  printf "Would you rather install Atom from: A) a Debian binary (faster) or B) from source (slower, but more reliable)? [A/B]"
+  printf "Would you rather install Atom from: A) a Debian binary (faster) or B) from source (slower, but more reliable)? [A/B] "
   read preference
+
   if [[ $preference == "A" ]]; then
-    if comex yaourt; then
-      yaourt -S atom-editor-bin --noconfirm
-    elif comex git; then
-      git clone $GIT/atom-editor-bin.git /tmp/atom-editor-bin
-      cd /tmp/atom-editor-bin
-      makepkg -si --noconfirm
-      cd .
-    elif comex curl; then
+    atomaur bin
   elif [[ $preference == "B" ]]; then
-
-    if comex yaourt; then                    # Install with yaourt if possible
-
-      yaourt -S atom-editor --noconfirm
-
-    elif comex git; then                      # Install with git and makepkg otherwise
-
-      git clone $GIT/atom-editor.git /tmp/atom-editor
-      atomin
-
-    elif comex curl; then                     # Install with curl and makepkg otherwise
-
-      curl -sL $AUR/atom-editor.tar.gz | tar xz -C /tmp
-      atomin
-
-    elif comex wget; then                     # Install with wget and makepkg otherwise
-
-      wget -cqO- $AUR/atom-editor.tar.gz | tar xz -C /tmp
-      atomin
-
-    else                                      # Install curl and install with curl and makepkg otherwise
-
-      sudo pacman -S curl --noconfirm
-      curl -sL $AUR/atom-editor.tar.gz | tar xz -C /tmp
-      cd /tmp/atom-editor
-      makepkg -si --noconfirm
-      cd .
-
-    fi
+    atomaur
+  fi
+  
 fi

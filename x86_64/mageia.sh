80744b5e1e069a7e45c7925e12f13a9ecdd54ea0
fusion809 <brentonhorne77@gmail.com>
2016-04-25 16:17:49 +1000

    Expanding 64-bit fed script

--- x86_64/mageia.sh
+++ x86_64/mageia.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# 64-bit RPM binary can be used to install Atom on Mageia, but it requires one to
+# uninstall the core program, apm as it conflicts with Atom's /usr/bin/apm executable
 mageia-build

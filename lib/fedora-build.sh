8662ccecc052b01923e01bd39195df5f56c8f8f1
fusion809 <brentonhorne77@gmail.com>
2016-04-25 13:22:58 +1000

    Expanding 64-bit fed script

---
+++ lib/fedora-build.sh
@@ -0,0 +1,7 @@
+function fedora-build {
+  # Get dependencies
+  sudo dnf copr enable nibbler/nodejs
+  sudo dnf install -y nodejs npm build-essential git \
+    libgnome-keyring-dev fakeroot
+  atom-build
+}
--- x86_64/fedora.sh
+++ x86_64/fedora.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+printf "Do you want to install Atom from A) a binary package (faster) or would you prefer B) a source install (slower)? [A/B] "
+read preference
+
+if [[ $preference == "A" ]]; then
+
+  curl -sL https://atom.io/download/rpm > /tmp/atom.x86_64.rpm         # Download latest binary
+  sudo dnf install -y /tmp/atom.x86_64.rpm                             # Install it with dpkg
+
+elif [[ $preference == "B" ]]; then
+
+  fedora-build
+
+fi

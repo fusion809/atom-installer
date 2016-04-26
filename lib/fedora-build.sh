b9adb67a4a7149642b1750fb8ac23ad07ed0e2cd
fusion809 <brentonhorne77@gmail.com>
2016-04-26 11:53:51 +1000

    Adding Fedora test

--- lib/fedora-build.sh
+++ lib/fedora-build.sh
@@ -2,8 +2,8 @@
 
 function fedora-build {
   # Get dependencies
-  sudo dnf copr enable nibbler/nodejs || sudo dnf install 'dnf-command(copr)' \
-    && sudo dnf copr enable nibbler/nodejs
+  sudo dnf copr enable nibbler/nodejs || sudo dnf install -y 'dnf-command(copr)' \
+    && sudo dnf copr enable -y nibbler/nodejs
   sudo dnf update
   sudo dnf install -y make gcc gcc-c++ glibc-devel git-core libgnome-keyring-devel
   atom-build

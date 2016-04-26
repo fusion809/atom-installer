0fb4cbf3f82999dfd65feccb0ad34621a0cac000
fusion809 <brentonhorne77@gmail.com>
2016-04-26 12:28:15 +1000

    Updating

--- lib/mageia-build.sh
+++ lib/mageia-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-. ./lib/atom-build.sh
-. ./lib/node-build.sh
 
 function mageia-build {
   # Get dependencies
   sudo urpmi git curl libgnome-keyring-devel make gcc gcc-c++
+  . ./lib/atom-build.sh
+  . ./lib/node-build.sh
   node-build
   atom-build
 }

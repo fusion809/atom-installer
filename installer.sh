cfb59354edc49bb9725854bc27cfe8eb9d97d259
fusion809 <brentonhorne77@gmail.com>
2016-04-26 12:19:49 +1000

    Expanding installer script and creating build-index.sh lib

--- installer.sh
+++ installer.sh
@@ -1,38 +1,31 @@
 #!/bin/bash
+# This script is the main script of this installer.
 
-# This script is the main script of this installer. It detects the operating system
-# and relevant hardware details.
-
+######################################################################################################
+# This first section determines if the user passed the option "help" to the installer script.
 if [[ "$1" == '-h' ]] || [[ "$1" == '--help' ]] || [[ "$1" == '?' ]]; then
   . ./help.sh
 fi
-# Distribution name
+######################################################################################################
+
+######################################################################################################
+# This section of the installer script detects the operating system
+# and relevant hardware details.
+
+# Distribution name. Previously lsb_release was used, but it failed on Docker containers so this
+# substitute was made.
 export LD=$(cat /etc/os-release | grep -w "NAME" | sed 's/NAME="//g' | sed 's/"//g')
 # Distribution architecture
 export ARCH=$(uname -m)
 # Distribution version number, e.g., on Fedora 23 it returns 23
 export VER=$(cat /etc/os-release | grep -w "VERSION_ID" | sed 's/VERSION_ID=//g' | sed 's/"//g')
 
-source "lib/test.sh"                        # Load the test functions
+######################################################################################################
+
+source "./lib/test.sh"                        # Load the test functions
 
 # Load the distribution-specific libraries
-if [[ $LD == "Arch Linux" ]] || [[ $LD == "Manjaro"* ]]; then
-  source "lib/atom-editor-aur.sh"
-elif [[ $LD == "CentOS"* ]]; then
-  source "lib/centos-build.sh"
-elif [[ $LD == "Debian"* ]]; then
-  source "lib/debian-build.sh"
-elif [[ $LD == "Fedora"* ]]; then
-  source "lib/fedora-build.sh"
-elif [[ $LD == "Linux Mint"* ]]; then
-  source "lib/linux-mint-build.sh"
-elif [[ $LD == "Mageia"* ]]; then
-  source "lib/mageia-build.sh"
-elif [[ $LD == "openSUSE"* ]]; then
-  source "lib/opensuse-build.sh"
-elif [[ $LD == "Ubuntu"* ]]; then
-  source "lib/ubuntu-build.sh"
-fi
+source "./lib/build-index.sh"
 
 DISTROS=(                                   # List of supported distributions
 'Arch'                                      #  Arch Linux

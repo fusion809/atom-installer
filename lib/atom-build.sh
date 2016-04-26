244e9c0f85371cf3de7722a4debef465c6e06e18
fusion809 <brentonhorne77@gmail.com>
2016-04-26 11:04:34 +1000

    Fixing CentOS bug

--- lib/atom-build.sh
+++ lib/atom-build.sh
@@ -73,7 +73,7 @@ function atom-build {
   script/build
   if [[ $DEST_TYPE == "local" ]]; then
     script/grunt install --channel=stable --install-dir $INST_DEST
-    printf "The Atom executable is now found at $INST_DEST/bin/atom \n"
+    printf "The Atom executable is now found at $SRC_DEST/$INST_DEST/bin/atom \n"
   elif [[ $DEST_TYPE = "system" ]]; then
     sudo script/grunt install --channel=stable --install-dir $INST_DEST
   fi

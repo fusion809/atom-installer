88a3a619a0e5baaf29d9517e5df69bb224f9605b
fusion809 <brentonhorne77@gmail.com>
2016-05-12 08:28:57 +1000

    updating

--- quick-config.sh
+++ quick-config.sh
@@ -5,8 +5,12 @@ elif which wget >/dev/null 2>&1; then
   /bin/bash -c "$(wget -cqO- https://git.io/vwEIX)"
 fi
 
+export AT=$HOME/.atom
+export GHUBM=$HOME/GitHub/mine
+export GHUBO=$HOME/GitHub/others
+
 # Change into config folder
-cd $HOME/.atom
+cd $AT
 
 # Initialize it as a git repo
 git init
@@ -19,3 +23,16 @@ git remote add origin git@github.com:fusion809/atom.git
 
 # Pull repository
 git pull origin master
+
+# Clone necessary repos
+git clone https://github.com/DanBrooker/file-icons $GHUBO/file-icons
+apm link $GHUBO/file-icons
+
+git clone git@github.com:fusion809/fusion-ui.git $GHUBM/fusion-ui
+apm link $GHUBM/fusion-ui
+
+git clone https://github.com/FortAwesome/Font-Awesome $AT/Font-Awesome
+git clone https://github.com/vorillaz/devicons $AT/devicons
+git clone https://github.com/Lukas-W/font-linux $AT/font-linux
+git clone https://github.com/fizzed/font-mfizz $AT/font-mfizz
+git clone https://github.com/driftyco/ionicons/ $AT/ionicons
--- quick-install.sh
+++ quick-install.sh
@@ -3,44 +3,84 @@
 # This is a quick install script to install Atom ASAP
 export REPO=https://github.com/fusion809/atom-installer
 export GHUB=$HOME/GitHub
+export GHUBM=$HOME/GitHub/mine
 
 if ! [[ -d $GHUB ]]; then
   mkdir $GHUB
 fi
 
-cd $GHUB
+if [[ -d $GHUBM ]]; then
+  cd $GHUBM
 
-if ! [[ -d $GHUB/atom-installer ]]; then                                                  # Get the repository, if necessary
-  printf "Getting the atom-installer repository locally ==>\n"
-  if which git >/dev/null 2>&1; then
-    git clone $REPO $GHUB/atom-installer
-  elif which curl >/dev/null 2>&1; then
-    curl -sL $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUB
-  elif which wget >/dev/null 2>&1; then
-    wget -cqO- $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUB
-  fi
-else
-  printf "The atom-installer repository is already locally present! "
-  printf "\n"
-  printf "Update your local copy? [y/n] "
-  read yn
-  if [[ $yn == "y" ]]; then
+  if ! [[ -d $GHUBM/atom-installer ]]; then                                                  # Get the repository, if necessary
+    printf "Getting the atom-installer repository locally ==>\n"
     if which git >/dev/null 2>&1; then
-      cd $GHUB/atom-installer
-      git pull origin master
-      cd .
+      git clone $REPO $GHUBM/atom-installer
     elif which curl >/dev/null 2>&1; then
-      rm -rf $GHUB/atom-installer
+      curl -sL $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUBM
+    elif which wget >/dev/null 2>&1; then
+      wget -cqO- $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUBM
+    fi
+  else
+    printf "The atom-installer repository is already locally present! "
+    printf "\n"
+    printf "Update your local copy? [y/n] "
+    read yn
+    if [[ $yn == "y" ]]; then
+      if which git >/dev/null 2>&1; then
+        cd $GHUBM/atom-installer
+        git pull origin master
+        cd .
+      elif which curl >/dev/null 2>&1; then
+        rm -rf $GHUBM/atom-installer
+        curl -sL $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUBM
+      elif which wget >/dev/null 2>&1; then
+        rm -rf $GHUBM/atom-installer
+        wget -cqO- $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUBM
+      fi
+    else
+      printf "Suit yourself!"
+    fi
+
+  fi
+
+  cd $GHUBM/atom-installer
+  ./installer.sh
+else
+  cd $GHUB
+
+  if ! [[ -d $GHUB/atom-installer ]]; then                                                  # Get the repository, if necessary
+    printf "Getting the atom-installer repository locally ==>\n"
+    if which git >/dev/null 2>&1; then
+      git clone $REPO $GHUB/atom-installer
+    elif which curl >/dev/null 2>&1; then
       curl -sL $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUB
     elif which wget >/dev/null 2>&1; then
-      rm -rf $GHUB/atom-installer
       wget -cqO- $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUB
     fi
   else
-    printf "Suit yourself!"
+    printf "The atom-installer repository is already locally present! "
+    printf "\n"
+    printf "Update your local copy? [y/n] "
+    read yn
+    if [[ $yn == "y" ]]; then
+      if which git >/dev/null 2>&1; then
+        cd $GHUB/atom-installer
+        git pull origin master
+        cd .
+      elif which curl >/dev/null 2>&1; then
+        rm -rf $GHUB/atom-installer
+        curl -sL $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUB
+      elif which wget >/dev/null 2>&1; then
+        rm -rf $GHUB/atom-installer
+        wget -cqO- $REPO/archive/master.tar.gz | tar xz --transform=s/atom-installer-master/atom-installer/ -C $GHUB
+      fi
+    else
+      printf "Suit yourself!"
+    fi
+
   fi
 
+  cd $GHUB/atom-installer
+  ./installer.sh
 fi
-
-cd $GHUB/atom-installer
-./installer.sh

6276d71122976668d193362faaa32259743e7e67
fusion809 <brentonhorne77@gmail.com>
2016-05-13 11:43:38 +1000

    Updating

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,9 @@
 # Script for easy pushing changes to repo
 ./perms.sh
 git add --all
-git commit -m "$1"
+if [[ -n $1 ]]; then
+  git commit -m "$1"
+else
+  git commit -m "Updating"
+fi
 git push origin master

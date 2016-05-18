f607fecf6a270f1d421d7ada0f10fa8cb5b1320a
fusion809 <brentonhorne77@gmail.com>
2016-05-19 05:52:30 +1000

    Fixing atom_install

--- lib/install.sh
+++ lib/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 function atom_install {
   if [[ $DEST_TYPE == "system" ]]; then
-    sudo script/grunt install --channel=stable -_install-dir $INST_DEST
+    sudo script/grunt install --channel=stable --install-dir $INST_DEST
   else
-    script/grunt install --channel=stable -_install-dir $INST_DEST
+    script/grunt install --channel=stable --install-dir $INST_DEST
     printf "The Atom executable is now found at $SRC_DEST/$INST_DEST/bin/atom \n"
   fi
 }

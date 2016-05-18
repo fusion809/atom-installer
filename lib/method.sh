c39a015277d5aede7900db95bfcd01e8e4a1607e
fusion809 <brentonhorne77@gmail.com>
2016-05-19 06:35:23 +1000

    Updating

--- lib/method.sh
+++ lib/method.sh
@@ -3,3 +3,5 @@ function method {
   printf "Do you want to install Atom from A) a binary package (faster and safer) or would you prefer B) a source install (slower)? \n[Available options: A/B. If left blank the default of option B will be used]\n"
   read method
 }
+
+export -f method

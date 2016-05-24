ea159e021b4c8f4b3b515abb36f0fbc3ce0b7d72
fusion809 <brentonhorne77@gmail.com>
2016-05-25 02:22:06 +1000

    [TE] Updating

--- lib/install.sh
+++ lib/install.sh
@@ -3,15 +3,15 @@ function atom_install {
   if [[ $DEST_TYPE == "system" ]]; then
     if `comex dpkg`; then
       script/grunt mkdeb
-      cd out
+      cd out/deb
       sudo dpkg -i *.deb
       sudo apt-get -f install
-    elif `comex yum`; then
-      script/grunt mkrpm
-      sudo yum install -y out/*.rpm
     elif `comex dnf`; then
       script/grunt mkrpm
-      sudo dnf install -y out/*.rpm
+      sudo dnf install -y out/rpm/*.rpm
+    elif `comex yum`; then
+      script/grunt mkrpm
+      sudo yum install -y out/rpm/*.rpm
     elif `comex zypper`; then
       script/grunt mkrpm
       sudo zypper in -y out/*.rpm

61f57197fd993e5562a361e9ae823d0fb24ed850
fusion809 <brentonhorne77@gmail.com>
2016-05-24 17:34:58 +1000

    Updating

--- lib/build/ubuntu.sh
+++ lib/build/ubuntu.sh
@@ -3,11 +3,15 @@
 
 function ubuntu_build {
   # Get dependencies
-  sudo apt-get install -y curl
-  if curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -; then
-    sudo apt-get install -y nodejs
-  else
-    node_build
+  curl -L "https://projects.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/nodejs" > /tmp/PKGBUILD
+  nodelver=$(sed -n 's/pkgver=//p' /tmp/PKGBUILD)
+  nodever=$(node --version | sed 's/v//g')
+  if ! [[ $nodever == $nodelver ]]; then
+    if curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -; then
+      sudo apt-get install -y nodejs
+    else
+      node_build
+    fi
   fi
   sudo apt-get install -y build-essential git libgnome-keyring-dev \
     fakeroot

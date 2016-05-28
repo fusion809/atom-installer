e83276c4cd8b966c7e62c2a2662fdf39b91d6bfd
fusion809 <brentonhorne77@gmail.com>
2016-05-28 19:41:34 +1000

    Updating

--- lib/install.sh
+++ lib/install.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 function atom_install {
-  cd $SRC_DEST/atom
   if [[ $DEST_TYPE == "system" ]]; then
     if `comex dpkg`; then
       script/grunt mkdeb
       if [[ $ARCH == "x86_64" ]]; then
-        sudo dpkg -i out/atom-${pkgver}-amd64.deb
+        sudo dpkg -i $HOME/GitHub/out/atom-${pkgver}-amd64.deb
       else
-        sudo dpkg -i out/atom-${pkgver}-i386.deb
+        sudo dpkg -i $HOME/GitHub/out/atom-${pkgver}-i386.deb
       fi
       sudo apt-get -f install
     elif `comex dnf`; then
       script/grunt mkrpm
-      sudo dnf install -y out/rpm/*.rpm
+      sudo dnf install -y $HOME/GitHub/out/rpm/*.rpm
     elif `comex yum`; then
       script/grunt mkrpm
-      sudo yum install -y out/rpm/*.rpm
+      sudo yum install -y $HOME/GitHub/out/rpm/*.rpm
     elif `comex zypper`; then
       script/grunt mkrpm
-      sudo zypper in -y out/rpm/*.rpm
+      sudo zypper in -y $HOME/GitHub/out/rpm/*.rpm
     else
       sudo script/grunt install --channel=stable --install-dir $INST_DEST
     fi

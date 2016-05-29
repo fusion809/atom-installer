1053efe5c53f2f115df6baa6e60479bf3bce00e4
fusion809 <brentonhorne77@gmail.com>
2016-05-29 23:13:57 +1000

    Updating

--- lib/pkg/platformio.sh
+++ lib/pkg/platformio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function sed_platformio {
   sed -i -e "/\"package-generator\": \".*\",/a \
-              \"platformio-ide\": \"${_platformio_ide_ver}\",\n    \"platformio-ide-terminal\": \"${_platformio_ide_terminal_ver}\"," package.json
+              \"platformio-ide-terminal\": \"${_platformio_ide_terminal_ver}\"," package.json
 }
 
 export -f sed_platformio

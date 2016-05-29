7e8d56c5ac5188726acc558e35acd017686065ac
fusion809 <brentonhorne77@gmail.com>
2016-05-29 22:40:52 +1000

    Adding build pers.

--- lib/install.sh
+++ lib/install.sh
@@ -2,22 +2,18 @@
 function atom_install {
   if [[ $DEST_TYPE == "system" ]]; then
     if `comex dpkg`; then
-
       if [[ $ARCH == "x86_64" ]]; then
-        script/grunt mkdeb && sudo dpkg -i out/atom-${pkgver}-amd64.deb
+        script/grunt mkdeb && sudo dpkg -i out/atom-${pkgver}-amd64.deb && sudo apt-get -f install
       else
-        script/grunt mkdeb && sudo dpkg -i out/atom-${pkgver}-i386.deb
+        script/grunt mkdeb && sudo dpkg -i out/atom-${pkgver}-i386.deb && sudo apt-get -f install
       fi
       sudo apt-get -f install
     elif `comex dnf`; then
-      script/grunt mkrpm
-      sudo dnf install -y $HOME/GitHub/out/rpm/*.rpm
+      script/grunt mkrpm && sudo dnf install -y out/rpm/atom*.rpm
     elif `comex yum`; then
-      script/grunt mkrpm
-      sudo yum install -y $HOME/GitHub/out/rpm/*.rpm
+      script/grunt mkrpm && sudo yum install -y out/rpm/atom*.rpm
     elif `comex zypper`; then
-      script/grunt mkrpm
-      sudo zypper in -y $HOME/GitHub/out/rpm/*.rpm
+      script/grunt mkrpm && sudo zypper in -y out/rpm/*.rpm
     else
       sudo script/grunt install --channel=stable --install-dir $INST_DEST
     fi

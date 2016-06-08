f1425b5e1e56075d0e93fb6bcc5bc42635b4820b
fusion809 <brentonhorne77@gmail.com>
2016-06-08 17:09:25 +1000

    Updating

--- lib/sed_pkg.sh
+++ lib/sed_pkg.sh
@@ -10,12 +10,13 @@ function sed_pkg {
 
   cd $SRC_DEST/atom
 
-  sed -i -e "/exception-reporting/d" \
-         -e "/metrics/d" \
-         -e "s/0.36.8/${_electron_ver}/g" \
-         -e "s/\"language-gfm\": \".*\",/\"language-gfm2\": \"${_language_gfm2_ver}\",\n    \"language-ini-desktop\": \"${_language_ini_desktop_ver}\",\n    \"language-liquid\": \"${_language_liquid_ver}\",\n    \"language-patch2\": \"${_language_patch2_ver}\",/g" \
+  sed -i -e "s/0.36.8/${_electron_ver}/g" \
+         -e "/\"language-gfm2\": \".*\",/a \
+              \"language-ini-desktop\": \"${_language_ini_desktop_ver}\"," \
+         -e "/\"language-ini-desktop\": \".*\",/a \
+              \"language-liquid\": \"${_language_liquid_ver}\",\n    \"language-patch2\": \"${_language_patch2_ver}\"," \
          -e "/\"dependencies\": {/a \
-                     \"language-patch2\": \"${_language_patch2_url}\"," \
+              \"language-patch2\": \"${_language_patch2_url}\"," \
          -e "s/\"language-shellscript\": \".*\",/\"language-unix-shell\": \"${_language_unix_shell_ver}\",\n    \"language-archlinux\": \"${_language_archlinux_ver}\",/g" \
          -e "s/\"about\": \".*\"/\"about-arch\": \"${_about_arch_ver}\"/g" \
          -e "/\"packageDependencies\": {/a \
@@ -25,7 +26,8 @@ function sed_pkg {
          -e "/base16/d" \
          -e "/solarized/d" \
          -e "/one-/d" \
-         package.json
+         -e "/exception-reporting/d" \
+         -e "/metrics/d" package.json
 }
 
 export -f sed_pkg

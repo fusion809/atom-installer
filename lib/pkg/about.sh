ca9379676bdf8609cff81e9d09531ba3ee5f9fb5
fusion809 <brentonhorne77@gmail.com>
2016-06-08 16:41:57 +1000

    Updating

--- lib/pkg/about.sh
+++ lib/pkg/about.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 function sed_about {
-  #modify about
-  sed -i -e "s/\"about\": \".*\"/\"about-arch\": \"${_about_arch_ver}\"/g" package.json
+
+  sed -i -e "s/\"about\": \".*\",/\"about-arch\": \"${_about_arch_ver}\",/g" package.json
+
 }
 
 export -f sed_about
--- lib/pkg/gfm.sh
+++ lib/pkg/gfm.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
 function sed_gfm {
-  sed -i -e "s/\"language-gfm\": \".*\",/\"language-gfm2\": \"${_language_gfm2_ver}\",/g" package.json # GFM2
+
+  sed -i -e "s\"language-gfm\": \".*\",/\"language-gfm2\": \"${_language_gfm2_ver}\",/g" package.json
+
 }
 
 export -f sed_gfm

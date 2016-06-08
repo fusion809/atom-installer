411199a9b579b687867d5ca763c5283dfde57b67
fusion809 <brentonhorne77@gmail.com>
2016-06-08 16:37:34 +1000

    Updating

--- lib/sed_pkg.sh
+++ lib/sed_pkg.sh
@@ -10,22 +10,31 @@ function sed_pkg {
 
   cd $SRC_DEST/atom
 
+  printf "About to sed about=>\n"
   sed_about
 
+  printf "About to sed electron=>\n"
   sed_elect
 
+  printf "About to sed privacy=>\n"
   sed_priv
 
+  printf "About to sed gfm2=>\n"
   sed_gfm
 
+  printf "About to sed liquid=>\n"
   sed_liquid
 
+  printf "About to sed unix shell=>\n"
   sed_shellscript
 
+  printf "About to sed archlinux=>\n"
   sed_archlinux
 
+  printf "About to sed patch2=>\n"
   sed_patch2
 
+  printf "About to sed ini-desktop=>\n"
   sed_ini
 
 }

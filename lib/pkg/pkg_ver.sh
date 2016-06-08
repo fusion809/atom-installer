40ed758ac7495482258677def281c7e482bc494e
fusion809 <brentonhorne77@gmail.com>
2016-06-08 17:31:07 +1000

    Updating

--- lib/pkg/pkg_ver.sh
+++ lib/pkg/pkg_ver.sh
@@ -15,16 +15,16 @@ function pkg_det {
 export -f pkg_det
 
 function pkg_ver {
-  _about_arch_ver=$(pkg_det fusion809/about)
-  _dark_bint_syntax_ver=$(pkg_det Murriouz/dark-bint-syntax)
-  _electron_ver=0.36.12
-  _fusion_ui_ver=$(pkg_det fusion809/fusion-ui)
-  _language_archlinux_ver=$(pkg_det fusion809/language-archlinux)
-  _language_gfm2_ver=$(pkg_det fusion809/language-gfm2)
-  _language_ini_desktop_ver=$(pkg_det fusion809/language-ini-desktop)
-  _language_liquid_ver=$(pkg_det puranjayjain/language-liquid)
-  _language_patch2_ver=$(pkg_det fusion809/language-patch2)
-  _language_unix_shell_ver=$(pkg_det fusion809/language-shellscript)
+  export _about_arch_ver=$(pkg_det fusion809/about)
+  export _dark_bint_syntax_ver=$(pkg_det Murriouz/dark-bint-syntax)
+  export _electron_ver=0.36.12
+  export _fusion_ui_ver=$(pkg_det fusion809/fusion-ui)
+  export _language_archlinux_ver=$(pkg_det fusion809/language-archlinux)
+  export _language_gfm2_ver=$(pkg_det fusion809/language-gfm2)
+  export _language_ini_desktop_ver=$(pkg_det fusion809/language-ini-desktop)
+  export _language_liquid_ver=$(pkg_det puranjayjain/language-liquid)
+  export _language_patch2_ver=$(pkg_det fusion809/language-patch2)
+  export _language_unix_shell_ver=$(pkg_det fusion809/language-shellscript)
 }
 
 export -f pkg_ver

8c95d61c09e58c74de890e56acdaa7e290d42219
fusion809 <brentonhorne77@gmail.com>
2016-06-08 18:46:48 +1000

    Updating

--- lib/sed_pkg.sh
+++ lib/sed_pkg.sh
@@ -10,24 +10,21 @@ function sed_pkg {
 
   cd $SRC_DEST/atom
 
-  sed -i -e "s/0.36.8/${_electron_ver}/g" \
-         -e "/\"language-gfm2\": \".*\",/a \
-              \"language-ini-desktop\": \"${_language_ini_desktop_ver}\"," \
-         -e "/\"language-ini-desktop\": \".*\",/a \
-              \"language-liquid\": \"${_language_liquid_ver}\",\n    \"language-patch2\": \"${_language_patch2_ver}\"," \
-         -e "/\"dependencies\": {/a \
-              \"language-patch2\": \"${_language_patch2_url}\"," \
-         -e "s/\"language-shellscript\": \".*\",/\"language-unix-shell\": \"${_language_unix_shell_ver}\",\n    \"language-archlinux\": \"${_language_archlinux_ver}\",/g" \
-         -e "s/\"about\": \".*\"/\"about-arch\": \"${_about_arch_ver}\"/g" \
-         -e "/\"packageDependencies\": {/a \
-              \"dark-bint-syntax\": \"${_dark_bint_syntax_ver}\",\n    \"fusion-ui\": \"${_fusion_ui_ver}\"," \
-         -e "/atom-dark/d" \
-         -e "/atom-light/d" \
-         -e "/base16/d" \
-         -e "/solarized/d" \
-         -e "/one-/d" \
-         -e "/exception-reporting/d" \
-         -e "/metrics/d" package.json
+  sed -i -e "/exception-reporting/d" \
+       -e "/metrics/d" \
+       -e "/atom-dark/d" \
+       -e "/atom-light/d" \
+       -e "/base16/d" \
+       -e "/solarized/d" \
+       -e "/one/d" \
+       -e "s/0.36.8/${_electron_ver}/g" \
+       -e "s/\"language-gfm\": \".*\",/\"language-gfm2\": \"${_language_gfm2_ver}\",\n    \"language-ini-desktop\": \"${_language_ini_desktop_ver}\",\n    \"language-liquid\": \"${_language_liquid_ver}\",\n    \"language-patch2\": \"${_language_patch2_ver}\",/g" \
+       -e "/\"dependencies\": {/a \
+                   \"language-patch2\": \"${_language_patch2_url}\"," \
+       -e "s/\"language-shellscript\": \".*\",/\"language-unix-shell\": \"${_language_unix_shell_ver}\",\n    \"language-archlinux\": \"${_language_archlinux_ver}\",/g" \
+       -e "s/\"about\": \".*\"/\"about-arch\": \"${_about_arch_ver}\"/g" \
+       -e "/\"packageDependencies\": {/a \
+            \"dark-bint-syntax\": \"${_dark_bint_syntax_ver}\",\n    \"fusion-ui\": \"${_fusion_ui_ver}\"," package.json
 }
 
 export -f sed_pkg

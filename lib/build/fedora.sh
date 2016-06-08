3cc5982487c05d9d0be5149fdd015fc29b485cc9
fusion809 <brentonhorne77@gmail.com>
2016-06-08 18:30:37 +1000

    Updating

--- lib/build/fedora.sh
+++ lib/build/fedora.sh
@@ -2,10 +2,8 @@
 
 function fedora_build {
   # Get dependencies
-  sudo dnf copr enable nibbler/nodejs || sudo dnf install -y 'dnf-command(copr)' \
-    && sudo dnf copr enable -y nibbler/nodejs
-  sudo dnf update
   sudo dnf install -y make gcc gcc-c++ glibc-devel git-core libgnome-keyring-devel tar nodejs npm rpmdevtools
+  sudo npm install -g npm@1.4.28 --loglevel error
   atom_build
 }
 

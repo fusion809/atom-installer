2cad4f30c996fe02e0ebe874c29ace93dc8623eb
fusion809 <brentonhorne77@gmail.com>
2016-06-12 20:59:03 +1000

    Updating

--- lib/sed_pkg.sh
+++ lib/sed_pkg.sh
@@ -28,8 +28,7 @@ function sed_pkg {
          -e "/language-gfm/d" \
          -e "/language-shellscript/d" \
          -e "s/0.36.8/0.36.12/g" \
-         -e "/\"language-yaml\": \".*\",/a \
-              \"dark-bint-syntax\": \"${_dark_bint_syntax_ver}\",\n    \"fusion-ui\": \"${_fusion_ui_ver}\",\n    \"language-archlinux\": \"${_language_archlinux_ver}\",\n    \"language-gfm2\": \"${_language_gfm2_ver}\",\n    \"language-ini-desktop\": \"${_language_ini_desktop_ver}\",\n    \"language-liquid\": \"${_language_liquid_ver}\",\n    \"language-patch2\": \"${_language_patch2_ver}\",\n    \"language-unix-shell\": \"${_language_unix_shell_ver}\",\n    \"terminal-fusion\": \"${_terminal_fusion_ver}\"," \
+         -e "s/\"language-yaml\": \".*\"/\"language-yaml\": \".*\",\n    \"dark-bint-syntax\": \"${_dark_bint_syntax_ver}\",\n    \"fusion-ui\": \"${_fusion_ui_ver}\",\n    \"language-archlinux\": \"${_language_archlinux_ver}\",\n    \"language-gfm2\": \"${_language_gfm2_ver}\",\n    \"language-ini-desktop\": \"${_language_ini_desktop_ver}\",\n    \"language-liquid\": \"${_language_liquid_ver}\",\n    \"language-patch2\": \"${_language_patch2_ver}\",\n    \"language-unix-shell\": \"${_language_unix_shell_ver}\",\n    \"terminal-fusion\": \"${_terminal_fusion_ver}\"/g" \
          -e "/\"dependencies\": {/a \
               \"language-patch2\": \"${_language_patch2_url}\"," $SRC_DEST/atom/package.json
 }

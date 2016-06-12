aca00c9932710b5c126284875905c7fe416b3851
fusion809 <brentonhorne77@gmail.com>
2016-06-12 22:37:16 +1000

    Updating

--- lib/install.sh
+++ lib/install.sh
@@ -3,11 +3,14 @@ function atom_install {
   if [[ $DEST_TYPE == "system" ]]; then
     if `comex dpkg`; then
       if [[ $ARCH == "x86_64" ]]; then
-        script/grunt mkdeb && sudo dpkg -i "$SRC_DEST/atom/out/atom-${pkgver}-amd64.deb" && sudo apt-get -f install
+        script/grunt mkdeb || die "Running script/grunt mkdeb failed! ==>\n"
+        printf "Going to attempt to install the Atom package built by script/grunt mkdeb at $SRC_DEST/atom/out/atom-${pkgver}-amd64.deb. ==>\n"
+        sudo dpkg -i "$SRC_DEST/atom/out/atom-${pkgver}-amd64.deb" && sudo apt-get -f install || die "Installing $SRC_DEST/atom/out/atom-${pkgver}-amd64.deb failed. You may need to run 'sudo dpkg -i $SRC_DEST/atom/out/atom-${pkgver}-amd64.deb && sudo apt-get -f install'"
       else
-        script/grunt mkdeb && sudo dpkg -i "$SRC_DEST/atom/out/atom-${pkgver}-i386.deb" && sudo apt-get -f install
+        script/grunt mkdeb || die "Running script/grunt mkdeb failed! ==>\n"
+        printf "Going to attempt to install the Atom package built by script/grunt mkdeb at $SRC_DEST/atom/out/atom-${pkgver}-i386.deb. ==>\n"
+        sudo dpkg -i "$SRC_DEST/atom/out/atom-${pkgver}-i386.deb" && sudo apt-get -f install || die "Installing $SRC_DEST/atom/out/atom-${pkgver}-i386.deb failed. You may need to run 'sudo dpkg -i $SRC_DEST/atom/out/atom-${pkgver}-i386.deb && sudo apt-get -f install'"
       fi
-      sudo apt-get -f install
     elif `comex dnf`; then
       script/grunt mkrpm && sudo dnf install -y "$SRC_DEST/atom/out/rpm/atom*.rpm"
     elif `comex yum`; then

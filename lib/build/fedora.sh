fa2ed71c04386df8435b4bcd7c41511195907ba1
fusion809 <brentonhorne77@gmail.com>
2016-06-22 01:38:18 +1000

    Adding patch dep

--- lib/build/fedora.sh
+++ lib/build/fedora.sh
@@ -3,7 +3,7 @@
 function fedora_build {
   printf "Running fedora_build in ./lib/build/fedora.sh. ==>\n"
   # Get dependencies
-  sudo dnf install -y make gcc gcc-c++ glibc-devel git-core libgnome-keyring-devel tar patch npm \
+  sudo dnf install -y make gcc gcc-c++ glibc-devel git-core libgnome-keyring-devel tar patch nodejs-devel \
     rpmdevtools alsa-lib GConf2-devel libXtst libnotify gtk2-devel python
   sudo npm install -g node-gyp
   atom_build

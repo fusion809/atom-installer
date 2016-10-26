73afecae4d8e6f657e83a75dc4738ea808d97de5
fusion809 <brentonhorne77@gmail.com>
2016-10-27 06:13:04 +1000

    UPdating

--- lib/install.sh
+++ lib/install.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 function atom_install {
-  if [[ $DEST_TYPE == "system" ]]; then
-    if `comex dpkg`; then
-      if [[ $ARCH == "x86_64" ]]; then
-        script/build --create-debian-package || die "Running script/build --create-debian-package failed! ==>\n"
-        printf "Going to attempt to install the Atom package built by script/grunt mkdeb at $SRC_DEST/atom/out/atom-${pkgver}-amd64.deb. ==>\n"
-        sudo dpkg -i "$SRC_DEST/atom/out/atom-${pkgver}-amd64.deb"
-        sudo apt-get -f install
-      else
-        script/build --create-debian-package || die "Running script/build --create-debian-package failed! ==>\n"
-        printf "Going to attempt to install the Atom package built by script/grunt mkdeb at $SRC_DEST/atom/out/atom-${pkgver}-i386.deb. ==>\n"
-        sudo dpkg -i "$SRC_DEST/atom/out/atom-${pkgver}-i386.deb"
-        sudo apt-get -f install
-      fi
-    elif `comex dnf`; then
-      script/build --create-rpm-package && sudo dnf install -y "$SRC_DEST/atom/out/rpm/atom*.rpm"
-    elif `comex yum`; then
-      script/build --create-rpm-package && sudo yum install -y "$SRC_DEST/atom/out/rpm/atom*.rpm"
-    elif `comex zypper`; then
-      script/build --create-rpm-package && sudo zypper in -y "$SRC_DEST/atom/out/rpm/*.rpm"
+  if `comex dpkg`; then
+    if [[ $ARCH == "x86_64" ]]; then
+      script/build --create-debian-package || die "Running script/build --create-debian-package failed! ==>\n"
+      printf "Going to attempt to install the Atom package built by script/grunt mkdeb at $SRC_DEST/atom/out/atom-${pkgver}-amd64.deb. ==>\n"
+      sudo dpkg -i "$SRC_DEST/atom/out/atom-${pkgver}-amd64.deb"
+      sudo apt-get -f install
+    else
+      script/build --create-debian-package || die "Running script/build --create-debian-package failed! ==>\n"
+      printf "Going to attempt to install the Atom package built by script/grunt mkdeb at $SRC_DEST/atom/out/atom-${pkgver}-i386.deb. ==>\n"
+      sudo dpkg -i "$SRC_DEST/atom/out/atom-${pkgver}-i386.deb"
+      sudo apt-get -f install
     fi
-  else
-    printf "Local install no longer supported!"
+  elif `comex dnf`; then
+    script/build --create-rpm-package && sudo dnf install -y "$SRC_DEST/atom/out/rpm/atom*.rpm"
+  elif `comex yum`; then
+    script/build --create-rpm-package && sudo yum install -y "$SRC_DEST/atom/out/rpm/atom*.rpm"
+  elif `comex zypper`; then
+    script/build --create-rpm-package && sudo zypper in -y "$SRC_DEST/atom/out/rpm/*.rpm"
   fi
 }
 
